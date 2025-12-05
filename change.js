d61d4ce0cd81b0c0ca9517dbf305cb18812f0dc2
pavankalyanarts <pavanteeda10@gmail.com>
2025-12-05 14:17:59 +0530

    Version 2

--- change.js
+++ change.js
@@ -1,1 +1,10 @@
-console.log("change new");
+console.log("change new");function obj(val){
+    if(val<=5){
+        return "baby is 5 months old"
+    }else{
+        return "as per the age, this one is no longer a baby"
+    }
+}
+
+let personCal = obj(5);
+console.log(personCal);
