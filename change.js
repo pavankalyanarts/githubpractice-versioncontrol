3b462dd1aef99cf379d8fe708b2b41a15210cbb2
pavankalyanarts <pavanteeda10@gmail.com>
2025-12-06 23:16:40 +0530

    version 12

--- change.js
+++ change.js
@@ -1,3 +1,29 @@
 console.log("change new");
 
 console.log("Version 1 updated")
+
+const people = [
+  { firstName: "John", lastName: "Doe", age: 28, favouriteColor: "Blue" },
+  { firstName: "Emily", lastName: "Stone", age: 22, favouriteColor: "Green" },
+  { firstName: "Arjun", lastName: "Rao", age: 30, favouriteColor: "Red" },
+  { firstName: "Priya", lastName: "Sharma", age: 26, favouriteColor: "Yellow" },
+  { firstName: "Mark", lastName: "Wilson", age: 33, favouriteColor: "Purple" },
+  { firstName: "Sara", lastName: "Lee", age: 24, favouriteColor: "Pink" },
+  { firstName: "Daniel", lastName: "Kim", age: 29, favouriteColor: "Black" },
+  { firstName: "Olivia", lastName: "Park", age: 21, favouriteColor: "Orange" }
+];
+
+// Optimized: Direct object creation without constructor overhead (more efficient for logging)
+people.forEach(person => {
+    const details = {
+        userFirstName: person.firstName,
+        userLastName: person.lastName,
+        userAge: person.age,
+        userFavouriteColor: person.favouriteColor
+    };
+    console.log(`${person.firstName}'s details: ${JSON.stringify(details)}`);
+})
+
+
+console.log("**Version 12**")
+
